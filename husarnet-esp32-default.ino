06b6680a1e7317a24e90d58c24e716ad23b96649
dominikn <dominik.nwk@gmail.com>
2020-05-15 09:11:53 +0200

    little fix

--- husarnet-esp32-default.ino
+++ husarnet-esp32-default.ino
@@ -27,7 +27,7 @@ const char* passwordTab[NUM_NETWORKS] = {
 
 // Husarnet credentials
 const char* hostName = "esp32websocket";  
-const char* husarnetJoinCode = "fc94:b01d:1803:8dd8:b293:5c7d:7639:932a/Tk72hzrLatV7rCCx5Qvgca";
+const char* husarnetJoinCode = "fc94:b01d:1803:8dd8:b293:5c7d:7639:932a/xxxxxxxxxxxxxxxxxxxxxx";
 
 #endif
 /* =============== config section end =============== */
